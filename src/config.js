6e90a9901ff4a942c78a9e9c61408ddabc886245
nose <nosecoolie@gmail.com>
2019-10-28 15:19:25 +0800

    edit endpoint domain

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_ENDPOINT = 'http://localhost:3010'
+const API_ENDPOINT = 'https://appointment-form-page.herokuapp.com/:3010'
 const CONSULTANT_TYPES = ['GP', 'Therapist', 'Specialist']
 const APPOINTMENT_TYPES = ['Video', 'Audio']
 
